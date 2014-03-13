13a13ab3b3f2a7ccd4652a24b39cad99a8d8e8df
scoudeimie <serge.coude@imie.fr>
2014-03-13 11:37:48 +0100

    Ajout de notre premier programme en C (Affichage et saisie clavier)

---
+++ c/ex1/ex1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int nbArg, char *tArg[]) {
+	//printf("nbArg : %i\n", nbArg);
+	if (nbArg >= 2) {
+		// On a au moins un argument passé
+		int i;
+		printf("Bonjour ");
+		for(i = 1; i < nbArg; i++) {
+			// Pour chaque argument passé au programme
+			if (i < nbArg-1) {
+				// je ne suis pas encore arrivé au dernier
+				// argument
+				printf("%s et ", tArg[i]);
+			} else {
+				// je suis arrivé au dernier argument
+				printf("%s", tArg[i]);
+			}
+		}
+		printf(" !\n");
+	} else {
+		// il n'y a pas d'argument passé
+		// on demande un prénom
+		char prenom[31]; // variable de type tableau de caractères contenant 31 élements pour 30 caractère
+		printf("Veuillez saisir votre pr\x82nom : ");
+		gets(prenom);
+		printf("Bonjour %s !\n", prenom);
+	}
+	
+	return 0;
+}
+
