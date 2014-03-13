453173a3304250c40edf698c4f9274fcd4cae68e
scoudeimie <serge.coude@imie.fr>
2014-03-13 14:21:21 +0100

    Mise en place du squelete du programme de calcul de la moyenne

---
+++ c/ex2/calcul-moyenne.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+float verificationSaisie(char *chaine) {
+	// algo de la fonction verificationSaisie
+
+}
+
+int main() {
+
+// Définition des variables globales
+
+// L'algorithme
+
+while(<cond>) {
+
+}
+
+}
