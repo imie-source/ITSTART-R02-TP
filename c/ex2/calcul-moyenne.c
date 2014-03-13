cc2fea5dce687be2b10162fa7b349bffc71fadd3
scoudeimie <serge.coude@imie.fr>
2014-03-13 16:15:19 +0100

    Codage de la fonction verificationSaisie

--- c/ex2/calcul-moyenne.c
+++ c/ex2/calcul-moyenne.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 float verificationSaisie(char *chaine) {
 	// Définition des variables locales
-	
+	int lngChaine;
+	int i;
+	char c;
 	// algo de la fonction verificationSaisie
-
+	lngChaine = strlen(chaine);
+	for(i = 0; i < lngChaine; i++) {
+		c = chaine[i];
+		if ( ( c < '0' ||  c > '9') &&
+		     ( c != '.' ) &&
+			 ( c != ',' ) ) {
+			 return -1;
+		}
+		// Remplacement de la virgule par un point
+		// pour la conversion en float via atof
+		if (c == ',') {
+			chaine[i] = '.';
+		}
+	}
+	return atof(chaine);
 }
 
 int main() {
@@ -12,6 +30,8 @@ int main() {
 // Définition des variables globales
 
 // L'algorithme
+
+	note = verificationSaisie(saisie);
 
 while(<cond>) {
 
