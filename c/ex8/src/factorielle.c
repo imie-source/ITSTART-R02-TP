1fc039b6e102ea7fbe268066bf0a46c6c8acc2e2
scoudeimie <serge.coude@imie.fr>
2014-03-21 13:54:59 +0100

    Passage en récursif de la fonction factorielle

--- c/ex8/src/factorielle.c
+++ c/ex8/src/factorielle.c
@@ -1,16 +1,19 @@
 #include "../include/factorielle.h"
 
 /**
- * Renvoie la factorielle d'un nombre
+ * Renvoie la factorielle d'un nombre (recursive)
  * 
  * @author Serge COUDÉ
  * @param int n Nombre à factorielliser
  * @return unsigned long Factorielle du nombre
  */
 unsigned long long factorielle(int n) {
-	unsigned long long res = 1;
-	for(; n > 1; n--) {
-		res *= n;
+	if (n <= 1) {
+		return 1;
 	}
-	return res;
+	return n * factorielle(n-1);
+	/*
+		notation "abrégée" :
+		return n <= 1 ? 1 : n * factorielle(n-1);
+	*/
 }
