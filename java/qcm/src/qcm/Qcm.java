0dab45b5b73983567efb409cd41786f9102cd0e2
scoudeimie <serge.coude@imie.fr>
2014-04-03 17:03:02 +0200

    Premier jet d'une question / réponse

--- java/qcm/src/qcm/Qcm.java
+++ java/qcm/src/qcm/Qcm.java
@@ -9,10 +9,14 @@ package qcm;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.StringTokenizer;
+import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import qcm.QuestionReponses;
 
 /**
  *
@@ -90,22 +94,58 @@ public class Qcm {
         }    
     }
     
-    public static void lireQCM() {
+    public static Vector lireQCM() {
         String fichier = "C:\\dev\\ITSTART-R02-TP\\java\\qcm\\build\\qcmchiffre.txt";
         Scanner scanner;
+        Vector tab = new Vector();
         try {
             scanner = new Scanner(new File(fichier));
-            int i = 1;
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 String ligne = Qcm.chiffre(-5, line);
                 StringTokenizer st = new StringTokenizer(ligne, ";");
-                System.out.println(i + " " + st.nextToken());
-                i++;
+                tab.add(new QuestionReponses(st.nextToken(), 
+                                             st.nextToken(), 
+                                             st.nextToken(), 
+                                             st.nextToken()));
             }
             scanner.close();
         } catch (FileNotFoundException ex) {
             //Logger.getLogger(Qcm.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return tab;
+    }
+    
+    public static int random(int bInf, int bSup) {
+        Random r = new Random();
+        return bInf + (Math.abs(r.nextInt()) % (bSup-bInf+1)); 
+    }
+    
+    public static void jouer(Vector t, int nbQ) {
+        for(int i = 0; i < nbQ; i++) {
+            QuestionReponses qr = (QuestionReponses)t.elementAt(i);
+            System.out.println(qr.question);
+            Vector tabChoix = new Vector();
+            int iBR = 0;
+            tabChoix.add(qr.bonneReponse);
+            tabChoix.add(qr.reponse2);
+            tabChoix.add(qr.reponse3);
+            for(int c = 0; c < 3; c++) {
+                int ia = Qcm.random(0, tabChoix.size()-1);
+                System.out.println("\t" + (c+1) + " : " + tabChoix.elementAt(ia));
+                if (tabChoix.elementAt(ia).equals(qr.bonneReponse)) {
+                    iBR = c+1;
+                }
+                tabChoix.removeElementAt(ia);
+            }
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Quel est votre choix ? (1/2/3) ");
+            String str = sc.nextLine();
+            if (Integer.parseInt(str) == iBR) {
+                System.out.println("Bonne réponse !");
+            } else {
+                System.out.println("Mauvaise réponse...");
+            }
         }
     }
     /**
@@ -121,7 +161,8 @@ public class Qcm {
         } else {
             System.out.println("Le fichier n'a pas été chiffré...");
         }*/
-        Qcm.lireQCM();
+        Vector tabQR = Qcm.lireQCM();
+        Qcm.jouer(tabQR, 1);
     }
     
 }
