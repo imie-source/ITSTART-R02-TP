451cefa52b0ace9fa9ca1b185a30e106de8978f2
scoudeimie <serge.coude@imie.fr>
2014-04-08 10:39:34 +0200

    Initialisation de l'algorithme principal du jeu de rôle

--- java/jdr/src/jdr/Jdr.java
+++ java/jdr/src/jdr/Jdr.java
@@ -6,6 +6,8 @@
 
 package jdr;
 
+import java.io.File;
+
 /**
  *
  * @author Administrateur
@@ -13,10 +15,36 @@ package jdr;
 public class Jdr {
 
     /**
+     * Analyse le fichier passé en argument et renvoie un objet dont le type
+     * dépend du contenu du fichier
+     *
+     * @param chemin Chemin du fichier à analyser
+     * @return \Object
+     */
+    public static Object analyse(String chemin) {
+        return null;
+    }
+    
+    /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        String repertoireData;
+        String fichierCourant; 
+        
+        if (2 == args.length) {
+            repertoireData = args[0];
+            fichierCourant = args[1];
+        } else {
+            System.out.println("usage : <repdata> <fichierdépart>");
+            System.exit(0);
+        }
+
+        while(true) {
+            Object noeud = Jdr.analyse(repertoireData + File.pathSeparator  + fichierCourant);
+            fichierCourant = noeud.action();
+        }    
     }
     
 }
