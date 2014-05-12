05fec2ab78dd76301bfdb309878efe9074163987
scoudeimie <serge.coude@imie.fr>
2014-05-12 17:02:52 +0200

    Mise à jour des objets

--- bdd/jdr/jdr-jeu-commun.sql
+++ bdd/jdr/jdr-jeu-commun.sql
@@ -29,3 +29,23 @@ INSERT INTO personnage (IdPersonnage, NomPersonnage, PvPersonnage) VALUES
 	(17, "Belle-Mère", 666),
 	(18, "Ivrogne", 8),
 	(19, "Serge COUDÉ", 10);
+	
+INSERT INTO objet (IdObjet, NomObjet, IdPersonnage) VALUES
+	(1, "Paquet de Riz", 2),
+	(2, "Epée", 6),
+	(3, "Bouteille", NULL),
+	(4, "Bilboquet", 16),
+	(5, "Tronçonneuse", 19),
+	(6, "Fiole",  NULL),
+	(7, "Livres", 5),
+	(8, "Armure", NULL),
+	(9, "Argents (10 écus)", 10),
+	(10, "Argents (5 écus)", NULL),
+	(11, "Argents (1 écu)", 11),
+	(12, "Bottes magiques", 10),
+	(13, "Gourdin", 7),
+	(14, "Serviette", NULL),
+	(15, "Carte", 15);
+	
+SET FOREIGN_KEY_CHECKS = 1;	
+	
