52f4a80f6f200f474476a2b626bab8281782c022
scoudeimie <serge.coude@imie.fr>
2014-06-03 12:26:54 +0200

    Ajout de l'heure dynamique (côté navigateur)

--- web/bdd/js/bdd.js
+++ web/bdd/js/bdd.js
@@ -14,3 +14,19 @@
 	else 
 		styleBChoix.visibility = 'hidden';	
 }
+
+function lZero(n) {
+	/*if (n < 10)
+		return "0" + n;
+	return n;*/
+	return n < 10 ? "0"+n : n;
+}
+
+function getDateNavigateur() {
+	var date = new Date();
+	var time = '';
+	time += lZero(date.getDate()) + '/' + lZero(date.getMonth()+1) + '/' + date.getFullYear() + ' ';
+	time += lZero(date.getHours()) + 'h' + lZero(date.getMinutes()) + '\'' + lZero(date.getSeconds()) + '\'\''; 
+	document.getElementById("dateHeureNavigateur").innerHTML = time;
+	//setTimeout("getDateNavigateur();", 900);
+}
